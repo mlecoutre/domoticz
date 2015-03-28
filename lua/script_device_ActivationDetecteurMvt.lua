80857e0eacf7acc0f494a6a97f398ba109a57573
mlecoutre <mlecoutre@gmail.com>
2015-03-28 10:41:43 +0100

    init mvt script

---
+++ lua/script_device_ActivationDetecteurMvt.lua
@@ -0,0 +1,8 @@
+
+ commandArray = {}
+ print('script test activation detecteur de mvt')
+ if (otherdevices['ActicationDetecteurMvt'] == 'On')) then
+    print('mvt !!')
+ else
+    print('pas de mvt.') 
+ end
